163ead47aceea5e0e5dec20b1760b6345e460597
jnt2811 <tranquangnguyen2811@gmail.com>
2022-06-22 20:09:04 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,80 +1,81 @@
 import "./App.css";
-import React, { useEffect, useState } from 'react';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-import ReactQuill from 'react-quill';
+import React, { useEffect, useState } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import ReactQuill from "react-quill";
 import "../node_modules/react-quill/dist/quill.snow.css";
 
-
 export default function App() {
-
   const {
     transcript,
     listening,
     resetTranscript,
-    browserSupportsSpeechRecognition
+    browserSupportsSpeechRecognition,
   } = useSpeechRecognition();
   const [body, setBody] = new useState();
   const [weight, setWeight] = new useState(0);
-  const [style, setStyle] = new useState('normal');
-  const [line, setLine] = new useState('none');
+  const [style, setStyle] = new useState("normal");
+  const [line, setLine] = new useState("none");
   // const [fontSize, setFontSize] = new useState(11);
 
   useEffect(() => {
-    setBody(transcript)
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    setBody(transcript);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [transcript]);
 
-  const handleBody = e => {
+  const handleBody = (e) => {
     console.log(e);
     setBody(e);
-  }
+  };
 
   function indam() {
-    if(weight==0) {
-      document.getElementById("demo").style.fontWeight= '900';
+    if (weight == 0) {
+      document.getElementById("demo").style.fontWeight = "900";
       setWeight(1);
-    }
-    else {
-      document.getElementById("demo").style.fontWeight= '300';
+    } else {
+      document.getElementById("demo").style.fontWeight = "300";
       setWeight(0);
     }
-    
   }
   function innghieng() {
-    if(style=='normal') {
-      document.getElementById("demo").style.fontStyle= 'italic';
-      setStyle('italic');
-    }
-    else {
+    if (style == "normal") {
+      document.getElementById("demo").style.fontStyle = "italic";
+      setStyle("italic");
+    } else {
       document.getElementById("demo").style.fontStyle = "normal";
-      setStyle('normal');
+      setStyle("normal");
     }
-    
   }
   function Gachchan() {
-    if(line=='none') {
-      document.getElementById("demo").style.textDecoration= 'underline';
-      setLine('italic');
+    if (line == "none") {
+      document.getElementById("demo").style.textDecoration = "underline";
+      setLine("italic");
+    } else if (line == "italic") {
+      document.getElementById("demo").style.textDecoration = "none";
+      setLine("none");
     }
-    else if(line=='italic') {
-      document.getElementById("demo").style.textDecoration= 'none';
-      setLine('none');
-    }
-    
   }
 
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
   }
 
-  console.log(transcript)
-
   return (
-    <div class="container">
-      <div class="header">
-          
-      </div>
-      <div class="container1">
+    <div class="containe">
+      <div class="container">
+        <div class="form" style={{ textAlign: "center" }}>
+          <h1>Hỗ trợ đánh văn bản Word</h1>
+
+          <p>Microphone: {listening ? "on" : "off"}</p>
+
+          <button onClick={SpeechRecognition.startListening}>Start</button>
+          <button onClick={SpeechRecognition.stopListening}>Stop</button>
+          <button onClick={resetTranscript}>Reset</button>
+        </div>
+
+        <br />
+
         <ReactQuill
           placeholder="Say somethings..."
           modules={App.modules}
@@ -82,51 +83,14 @@ export default function App() {
           onChange={handleBody}
           value={body}
         />
-          <div class="form">
-              <h1>Hỗ trợ đánh văn bản Word</h1>
-              <div class="option">
-                  <button class="option1" onClick={indam}>In đậm</button>
-                  <button class="option1" onClick={innghieng}>In nghiêng</button>
-                  <button class="option1" onClick={Gachchan}>Gạch chân</button>
-                  <button class="option1" >Chữ thường</button>
-              </div>
-              <p>Microphone: {listening ? 'on' : 'off'}</p>
-              <input
-              class="input"
-              type="textarea"
-              name="text"
-              id="textInput"
-              value={transcript}
-              
-              placeholder="Your say..."
-              
-            />              
-            <div class="sub-form" >
-                  <input class="input2" placeholder="Text" id="demo" />
-                  <p id="temp" >aaa</p>
-            </div>
-            <button onClick={SpeechRecognition.startListening}>Start</button>
-            <button onClick={SpeechRecognition.stopListening}>Stop</button>
-            <button onClick={resetTranscript}>Reset</button>
-              <button class="msger-mic-btn">
-                <img
-                  src="{{ url_for('static', filename='styles/icons8-microphone-30.png') }}"
-                  alt=""
-                  srcset=""
-                  class="image-mic"
-                />
-              </button>              
-              <button class="picture">Export to Word</button>
-          </div>
       </div>
-      
-  </div>
+    </div>
   );
 }
 
 App.modules = {
   toolbar: [
-    [{  header: "1" }, { header: "2" }, { header: [3, 4, 5, 6] }, { font: [] }],
+    [{ header: "1" }, { header: "2" }, { header: [3, 4, 5, 6] }, { font: [] }],
     [{ size: [] }],
     ["bold", "italic", "underline", "strike", "blockquote"],
     [{ list: "ordered" }, { list: "bullet" }],
